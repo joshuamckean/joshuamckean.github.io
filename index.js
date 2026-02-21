bc09eaf0a4ece8e00cc059990122ce64f8375ca8
Joshua McKean <joshuamckean1@gmail.com>
2026-02-21 14:33:25 +0000

    2.0 - Added landing page, begin building new site

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Listening at localhost:${port}`)
+})
+
+// Routes
+
+// Index page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+// Private dev page - Component playground
+app.get('/components', (req, res) => {
+  res.sendFile(__dirname + '/dev_files/component_playground.html')
+})
+
