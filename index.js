a6515f62158b2a29c09c57d8d68ad0dadb15fbaa
Joshua McKean <joshuamckean1@gmail.com>
2026-02-22 15:05:58 +0000

    Redesign home page, add personalised info, added new pages with info to be filled out

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+// About Me page
+app.get('/about-me', (req, res) => {
+  res.sendFile(__dirname + '/pages/about-me.html')
+})
+
+// Projects page
+app.get('/projects', (req, res) => {
+  res.sendFile(__dirname + '/pages/projects.html')
+})
+
+// Contact page
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/pages/contact.html')
+})
 
 // Private dev page - Component playground
 app.get('/components', (req, res) => {
